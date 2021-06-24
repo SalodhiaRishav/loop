6527da7e621e29d3a48f786a8b9acdfd9d207c84
Rishav Salodhia <rishav.salodhia@nagarro.com>
2021-06-24 17:22:51 +0530

    InitialCreate

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      :src="imgLink1"
+      height="100"
+      width="100">
+      <img
+      :src="imgLink2"
+      height="100"
+      width="100">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      imgLink1: null,
+      imgLink2: null
+    }
+  },
+  mounted () {
+    setInterval(() => {
+
+    }, 500)
   }
 }
 </script>
